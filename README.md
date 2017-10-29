50108295a5f4ddedeb916ea7108ab331a5477fd3
Thanos Diamantidis <thanosdi@live.com>
2017-10-29 18:35:31 +0200

    Refactored things.

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ Fetch streams from piratebay
 ## Add addon
 Open stremio, go to addons and on the bottom of the page add `http://localhost:7000/stremioget/stremio/v1` as a `Addon Repository Url`
 and then install as normal.
+
+Alternatively use `https://piratebay-stremio-addon.herokuapp.com/` as repository url if you dont want to run the add on locally.
--- index.js
+++ index.js
@@ -1,78 +1,85 @@
 const PirateBay = require('thepiratebay');
 const Stremio = require('stremio-addons');
 const magnet = require('magnet-uri');
-const tnp = require('torrent-name-parser');
 const imdb = require('imdb');
-
-process.env.STREMIO_LOGGING = true; // enable server logging for development purposes
 
 const manifest = {
 	// See https://github.com/Stremio/stremio-addons/blob/master/docs/api/manifest.md for full explanation
-	"id": "org.stremio.piratebay",
-	"version": "1.0.0",
-	"name": "PirateBay Addon",
-	"description": "Fetch PirateBay entries on a single episode.",
-	"icon": "https://files.gamebanana.com/img/ico/sprays/apirateslifeforme2007tpbpicrip.png",
-	"logo": "https://files.gamebanana.com/img/ico/sprays/apirateslifeforme2007tpbpicrip.png",
-	"isFree": true,
-	"contactEmail": "thanosdi@live.com",
-	"endpoint": "https://piratebay-stremio-addon.herokuapp.com/",
+	'id': 'org.stremio.piratebay',
+	'version': '1.0.0',
+	'name': 'PirateBay Addon',
+	'description': 'Fetch PirateBay entries on a single episode.',
+	'icon': 'https://files.gamebanana.com/img/ico/sprays/apirateslifeforme2007tpbpicrip.png',
+	'logo': 'https://files.gamebanana.com/img/ico/sprays/apirateslifeforme2007tpbpicrip.png',
+	'isFree': true,
+	'email': 'thanosdi@live.com',
+	'endpoint': 'https://piratebay-stremio-addon.herokuapp.com/',
 	// Properties that determine when Stremio picks this add-on
-	"types": ["movie", "series"], // your add-on will be preferred for those content types
-	"idProperty": "imdb_id", // the property to use as an ID for your add-on; your add-on will be preferred for items with that property; can be an array
+	'types': ['movie', 'series'], // your add-on will be preferred for those content types
+	'idProperty': 'imdb_id', // the property to use as an ID for your add-on; your add-on will be preferred for items with that property; can be an array
 	// We need this for pre-4.0 Stremio, it's the obsolete equivalent of types/idProperty
-	"filter": { "query.imdb_id": { "$exists": true }, "query.type": { "$in":["series","movie"] } }
+	'filter': { 'query.imdb_id': { '$exists': true }, 'query.type': { '$in':['series','movie'] } }
 };
 
-const methods = { };
 const addon = new Stremio.Server({
 	'stream.find': function(args, callback) {
-		if (args.query.type === 'series') {
-			imdb(args.query.imdb_id, function(err, data) {
-				if(err) {
-					console.log(err.stack);
-				}
-				if(data){
-					const {title} = data;
-					PirateBay.search(`${title} s0${args.query.season}e0${args.query.episode}`, {
-						orderBy: 'seeds',
-						sortBy: 'desc'
-					}).then((results) => {
-						return callback(null, results.slice(0, 3).map( episode => {
-							const {infoHash, announce } = magnet.decode(episode.magnetLink);
-							const availability = episode.seeders == 0 ? 0 : episode.seeders < 5 ? 1 : 2;
+		imdb(args.query.imdb_id, function(err, data) {
+			if(err) {
+				console.error(err.stack);
+			}
+			if(data){
+				const title = createTitle(data.title, args);
+				PirateBay.search(title, {
+					orderBy: 'seeds',
+					sortBy: 'desc'
+				}).then((results) => {
+					return callback(null, results.slice(0, 4).map( episode => {
+						const {infoHash, announce } = magnet.decode(episode.magnetLink);
+						const availability = episode.seeders == 0 ? 0 : episode.seeders < 5 ? 1 : 2;
 
-							const { resolution = 'SD', quality, audio, group } = tnp(title);
-							const detail = [ episode.name, quality, audio, group ].filter(val => val).join('' - '');
+						const detail = `${episode.name} S:${episode.seeders}`;
 
-							return {
-								infoHash,
-								name: 'PTB',
-								title: detail,
-								isFree: true,
-								sources: [...announce.map(src => `tracker:${src}`), `dht:${infoHash}`],
-								availability
-							};
-						}).filter(elem => elem.availability > 0));
-					}).catch((err) => {
-						console.error(err);
-						return callback(new Error('internal'));
-					});
-				}
-			});
-		}
+						return {
+							infoHash,
+							name: 'PTB',
+							title: detail,
+							isFree: true,
+							sources: [...announce.map(src => `tracker:${src}`), `dht:${infoHash}`],
+							availability
+						};
+					}).filter(elem => elem.availability > 0));
+				}).catch((err) => {
+					console.error(err);
+					return callback(new Error('internal'));
+				});
+			}
+		});
 	}
 }, manifest);
 
-const server = require('http').createServer((req, res) => {
-	addon.middleware(req, res, function() { res.end() }); // wire the middleware - also compatible with connect / express
-}).on('listening', () => {
-	console.log(`Sample Stremio Addon listening on ${server.address().port}`);
-	/* Methods
-	 */
-	methods["stream.find"] = function(args, callback) {
-		if (! args.query) return callback();
-		callback(null, [dataset[args.query.imdb_id]]);
+const createTitle = (movieTitle, args) => {
+	let title = movieTitle;
+	if (args.query.type === 'series') {
+		let season = args.query.season;
+		let episode = args.query.episode;
+
+		if (parseInt(season) < 10){
+			season = `0${season}`;
+		}
+		if (parseInt(episode) < 10){
+			episode = `0${episode}`;
+		}
+
+		title = `${movieTitle} S${season}E${episode}`;
 	}
 
-}).listen(process.env.PORT || 7000);
+	return title;
+};
+
+const server = require('http').createServer((req, res) => {
+	addon.middleware(req, res, function() { res.end() }); // wire the middleware - also compatible with connect / express
+})
+	.on('listening', () => {
+		console.log(`Piratebay Stremio Addon listening on ${server.address().port}`);
+	})
+	.listen(process.env.PORT || 7000);
